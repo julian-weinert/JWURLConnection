bdd0d4311e49e9229180004efde57a2c548597b7
Patrick <petersen.p@gmx.net>
2014-04-09 16:59:22 +0200

    Added Licence

--- JWURLConnection.m
+++ JWURLConnection.m
@@ -1,10 +1,21 @@
 //
-//  JWURLConnection.m
-//  t
+//  JWURLConnection.h
 //
 //  Created by Julian Weinert on 07.01.13.
-//  Copyright (c) 2013 Julian Weinert. All rights reserved.
+//  Copyright (c) 2013 Julian Weinert.
 //
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 static NSMutableArray *trustedHosts;
 
